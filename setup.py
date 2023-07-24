25c6af46bff236b87035f01d404793df7bba88df
Holt Skinner <holtskinner@google.com>
2023-07-24 11:57:27 -0500

    chore: Update min python client library for documentai

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setuptools.setup(
         "proto-plus >= 1.22.2, <2.0.0dev; python_version>='3.11'",
         "grpc-google-iam-v1 >= 0.12.4, < 0.13dev",
         "google-cloud-bigquery >= 3.5.0, < 4.0.0dev",
-        "google-cloud-documentai >= 1.2.1, < 3.0.0dev",
+        "google-cloud-documentai >= 2.17.0, < 3.0.0dev",
         "google-cloud-storage >= 1.31.0, < 3.0.0dev",
         "google-cloud-vision >= 2.7.0, < 4.0.0dev ",
         "numpy >= 1.18.1",
